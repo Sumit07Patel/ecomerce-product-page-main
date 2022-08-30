2f93d57361568ebec6e51c88a3dd61fa25f6def8
Sumit07Patel <sumitpatel7103@gmail.com>
2022-08-31 02:53:40 +0530

    custom carosel

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,5 @@
+
+//for hamburger menu
 const hamburgerMenu = document.querySelector('.hamburger-menu');
 const hamburgerBar = document.querySelectorAll('.hamburger-bar');
 const navLinks = document.querySelector('.nav-links');
@@ -10,3 +12,49 @@ hamburgerMenu.addEventListener("click", ()=>{
         hamburgerBar[i].classList.toggle('close-menu');
     }
 })
+
+
+
+//for the carosel
+
+
+
+const caroselSlide = document.querySelector('.item-img-carosel');
+const caroselImg = document.querySelectorAll('.item-img-carosel img');
+const previousButton = document.querySelector('.previous-button');
+const nextButton = document.querySelector('.next-button');
+
+let counter = 1;
+const size = caroselImg[0].clientWidth;
+
+caroselSlide.style.transform = 'translateX('+(-size*counter)+'px)';
+
+//button listeners
+nextButton.addEventListener('click', ()=>{
+    if(counter >= caroselImage.length) return;
+    caroselSlide.classList.add('transition');
+    counter++;
+    caroselSlide.style.transform = 'translateX('+(-size*counter)+'px)';
+})
+previousButton.addEventListener('click', ()=>{
+    if(counter <= 0) return;
+    caroselSlide.classList.add('transition');
+    counter--;
+    caroselSlide.style.transform = 'translateX('+(-size*counter)+'px)';
+})
+caroselSlide.addEventListener('transitionend', ()=>{
+    if(caroselImg[counter].id === 'lastClone'){
+        caroselSlide.classList.remove('transition');
+        counter = caroselImg.length - 2;
+        caroselSlide.style.transform = 'translateX('+(-size*counter)+'px)';
+    }
+    if(caroselImg[counter].id === 'firstClone'){
+        caroselSlide.classList.remove('transition');
+        counter = caroselImg.length - counter;
+        caroselSlide.style.transform = 'translateX('+(-size*counter)+'px)';
+    }
+})
+
+
+
+
