b4d750f4c42ded7d6c8ab5ba7e0381db455cfd76
Sumit07Patel <sumitpatel7103@gmail.com>
2022-08-31 12:23:05 +0530

    fixed the bug in the carosel

--- javascript/main.js
+++ javascript/main.js
@@ -24,36 +24,57 @@ const caroselImg = document.querySelectorAll('.item-img-carosel img');
 const previousButton = document.querySelector('.previous-button');
 const nextButton = document.querySelector('.next-button');
 
-let counter = 1;
-const size = caroselImg[0].clientWidth;
-
-caroselSlide.style.transform = 'translateX('+(-size*counter)+'px)';
-
-//button listeners
-nextButton.addEventListener('click', ()=>{
-    if(counter >= caroselImg.length) return;
-    caroselSlide.classList.add('transition');
-    counter++;
+function carosel(){
+    let counter = 1;
+    let size = caroselImg[0].clientWidth;
     caroselSlide.style.transform = 'translateX('+(-size*counter)+'px)';
-})
-previousButton.addEventListener('click', ()=>{
-    if(counter <= 0) return;
-    caroselSlide.classList.add('transition');
-    counter--;
-    caroselSlide.style.transform = 'translateX('+(-size*counter)+'px)';
-})
-caroselSlide.addEventListener('transitionend', ()=>{
-    if(caroselImg[counter].id === 'lastClone'){
-        caroselSlide.classList.remove('transition');
-        counter = caroselImg.length - 2;
+
+    //button listeners
+    nextButton.addEventListener('click', ()=>{
+        if(counter >= caroselImg.length-1) return;
+        caroselSlide.classList.add('transition');
+        counter++;
         caroselSlide.style.transform = 'translateX('+(-size*counter)+'px)';
-    }
-    if(caroselImg[counter].id === 'firstClone'){
-        caroselSlide.classList.remove('transition');
-        counter = caroselImg.length - counter;
+    })
+    previousButton.addEventListener('click', ()=>{
+        if(counter <= 0) return;
+        caroselSlide.classList.add('transition');
+        counter--;
         caroselSlide.style.transform = 'translateX('+(-size*counter)+'px)';
-    }
+    })
+    caroselSlide.addEventListener('transitionend', ()=>{
+        if(caroselImg[counter].id === 'lastClone'){
+            caroselSlide.classList.remove('transition');
+            counter = caroselImg.length - 2;
+            caroselSlide.style.transform = 'translateX('+(-size*counter)+'px)';
+        }
+        if(caroselImg[counter].id === 'firstClone'){
+            caroselSlide.classList.remove('transition');
+            counter = caroselImg.length - counter;
+            caroselSlide.style.transform = 'translateX('+(-size*counter)+'px)';
+        }
+    })
+     return 0;
+}
+carosel();
+
+//When the screen size is changed
+window.addEventListener('resize', ()=>{
+    carosel();
 })
+
+//for desktop product interface
+
+const thumbnailImg = document.querySelectorAll('.thumbnail-item-img img');
+var newCounter;
+for(i=0; i<thumbnailImg.length; i++){
+    thumbnailImg[i].addEventListener('click', ()=>{
+        if(counter >= caroselImg.length-1) return;
+        caroselSlide.classList.add('transition');
+        counter= i+1;
+        caroselSlide.style.transform = 'translateX('+(-size*counter)+'px)';
+    })
+}
 
 
 
